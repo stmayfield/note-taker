7d94e8076fc9ddbb64726c6d151b44371e5bfeeb
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-06-20 15:55:48 -0700

    additional routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,52 @@
 const path = require("path");
-const db = require("../db/db.json")
-
-module.exports = (app) => {
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-    app.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-
-    app.get("/api/notes", (req, res) => {
-        return res.json(db);
-    });
-
-    app.post("/api/notes", (req, res) => {
-        let newNote = req.body;
-        db.push(newNote)
-        res.json(db)
-    });
+const fs = require("fs");
+const db = require("../db/db.json");
+const app = require("express").Router();
 
 
-};
 
+// HTML
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// API
+app.get("/api/notes", (req, res) => {
+    console.log(req.body)
+    return res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    console.log(newNote)
+    db.push(newNote)
+
+
+
+    for (let i = 0; i < db.length; i++) {
+        let noteID = db[i];
+        console.log(noteID)
+    }
+    return res.json(db)
+
+});
+
+
+
+
+
+app.delete("/api/notes/:id", (req, res) => {
+    let newNote = req.body;
+    const id = req.params.id;
+    console.log(id)
+    db.remove({ id: id }, res.json(db))
+});
+
+
+module.exports = app;
 
 
--- server.js
+++ server.js
@@ -6,8 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/routes")(app);
 
+app.use(require("./routes/routes"))
+// require("./routes/routes")(app);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
